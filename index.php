6a5f8209dede4d9193743c8ad490006f6804310d
Emmanuel Santiz <37223335+EmmanuelSantiz@users.noreply.github.com>
2019-12-23 18:14:38 -0600

    add super in index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ require_once 'lib/Database.php';
 require_once 'lib/Controller.php';
 require_once 'lib/View.php';
 require_once 'lib/Model.php';
+require_once 'lib/Supermodel.php';
 require_once 'lib/App.php';
 require_once 'lib/Helper.php';
 
