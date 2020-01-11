ab4ae02366957a6ecf0456382bb4a95ae130ee88
Emmanuel Santiz lenovo <emmanuel_fake_01@hotmail.com>
2020-01-11 09:15:29 -0600

    update bd

--- bd/curso(1).sql
+++ bd/curso(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 21, 2019 at 09:06 PM
+-- Generation Time: Jan 11, 2020 at 04:13 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.1.33
 
@@ -25,6 +25,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `ape_pat` varchar(50) NOT NULL,
+  `ape_mat` varchar(50) NOT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nombre`, `ape_pat`, `ape_mat`, `activo`) VALUES
+(1, 'Pedro', 'Cruz', 'Hernandez', 1),
+(2, 'Juan', 'Cruz', 'Lopez', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `detalle_factura`
 --
 
@@ -33,16 +55,20 @@ CREATE TABLE `detalle_factura` (
   `id_factura` int(11) NOT NULL,
   `id_producto` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
-  `subtotal` int(11) NOT NULL
+  `subtotal` int(11) NOT NULL,
+  `precio` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `detalle_factura`
 --
 
-INSERT INTO `detalle_factura` (`id`, `id_factura`, `id_producto`, `cantidad`, `subtotal`) VALUES
-(1, 11, 1, 1, 5),
-(2, 11, 2, 1, 20);
+INSERT INTO `detalle_factura` (`id`, `id_factura`, `id_producto`, `cantidad`, `subtotal`, `precio`) VALUES
+(1, 11, 1, 1, 5, '0.00'),
+(2, 11, 2, 1, 20, '0.00'),
+(3, 13, 1, 1, 5, '5.00'),
+(4, 13, 2, 1, 20, '20.00'),
+(5, 13, 3, 1, 50, '50.00');
 
 -- --------------------------------------------------------
 
@@ -74,7 +100,10 @@ INSERT INTO `factura` (`id`, `fecha`, `total`, `subtotal`, `estado`, `cliente`) 
 (8, '2019-12-21 00:00:00', '25.00', '25.00', 'pendiente', '1'),
 (9, '2019-12-21 00:00:00', '25.00', '25.00', 'pendiente', '1'),
 (10, '2019-12-21 00:00:00', '25.00', '25.00', 'pendiente', '1'),
-(11, '2019-12-21 00:00:00', '25.00', '25.00', 'pendiente', '1');
+(11, '2019-12-21 00:00:00', '25.00', '25.00', 'pendiente', '1'),
+(12, '2020-01-04 00:00:00', '75.00', '75.00', 'pendiente', '1'),
+(13, '2020-01-04 00:00:00', '75.00', '75.00', 'pendiente', '1'),
+(14, '2020-01-10 00:00:00', '0.00', '0.00', 'pagado', '1');
 
 -- --------------------------------------------------------
 
@@ -166,9 +195,9 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nombre`, `password`, `activo`, `email`, `created`) VALUES
 (1, 'test', 'b59c67bf196a4758191e42f76670ceba', 1, 'prueba@prueba.com1', '2019-12-06 23:26:52'),
-(2, 'emmanuel', '0d0de813c1105498e3435dd2fbf7fa26', 0, 'prueba@prueba.com', '2019-12-06 23:27:50'),
+(2, 'emmanuel', '0d0de813c1105498e3435dd2fbf7fa26', 1, 'prueba@prueba.com', '2019-12-06 23:27:50'),
 (5, 'emmanuel', '0d0de813c1105498e3435dd2fbf7fa26', 0, 'prueba', '2019-12-06 23:38:32'),
-(6, 'nuevo', 'd3eb9a9233e52948740d7eb8c3062d14', 1, 'nuevo@nuevo.com', '2019-12-07 11:38:36');
+(6, 'nuevo', 'd3eb9a9233e52948740d7eb8c3062d14', 0, 'nuevo@nuevo.com', '2019-12-07 11:38:36');
 
 -- --------------------------------------------------------
 
@@ -194,6 +223,12 @@ INSERT INTO `usuarios_roles` (`id`, `id_usuario`, `id_rol`, `activo`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `detalle_factura`
@@ -242,16 +277,22 @@ ALTER TABLE `usuarios_roles`
 --
 
 --
+-- AUTO_INCREMENT for table `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `detalle_factura`
 --
 ALTER TABLE `detalle_factura`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `factura`
 --
 ALTER TABLE `factura`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `productos`
